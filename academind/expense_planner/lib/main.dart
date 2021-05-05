a83b72678f017b1794ee6dcfbc62709efd98e60c
Jeremy Simpson <jeremy@accur8software.com>
2021-05-05 12:38:18 -0400

    section 5 module 129 complete * simple way to show content based on overall size of current screen

--- academind/expense_planner/lib/main.dart
+++ academind/expense_planner/lib/main.dart
@@ -66,48 +66,48 @@ class _MyHomePageState extends State<MyHomePage> {
   final amountController = TextEditingController();
 
   final List<Transaction> _userTransactions = [
-    // Transaction(
-    //   id: "t1",
-    //   title: "New Shoes",
-    //   amount: 69.99,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t2",
-    //   title: "Weekly Groceries",
-    //   amount: 75.25,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t3",
-    //   title: "Various stuff",
-    //   amount: 75.25,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t4",
-    //   title: "Whatever",
-    //   amount: 75.25,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t5",
-    //   title: "Blah blah",
-    //   amount: 75.25,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t6",
-    //   title: "Whatever",
-    //   amount: 75.25,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t7",
-    //   title: "Blah blah",
-    //   amount: 75.25,
-    //   date: DateTime.now(),
-    // ),
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Weekly Groceries",
+      amount: 75.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Various stuff",
+      amount: 75.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t4",
+      title: "Whatever",
+      amount: 75.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t5",
+      title: "Blah blah",
+      amount: 75.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t6",
+      title: "Whatever",
+      amount: 75.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t7",
+      title: "Blah blah",
+      amount: 75.25,
+      date: DateTime.now(),
+    ),
   ];
 
   bool _showChart = false;
@@ -162,8 +162,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text(
         "Personal Expenses",
@@ -176,9 +176,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final Widget txListWidget = Container(
-      height: (MediaQuery.of(context).size.height -
+      height: (mediaQuery.size.height -
               appBar.preferredSize.height -
-              MediaQuery.of(context).padding.top) *
+              mediaQuery.padding.top) *
           0.7,
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
@@ -206,9 +206,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             if (!isLandscape)
               Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                         appBar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
+                        mediaQuery.padding.top) *
                     0.3,
                 child: Chart(_recentTransactions),
               ),
@@ -216,9 +216,9 @@ class _MyHomePageState extends State<MyHomePage> {
             if (isLandscape)
               _showChart
                   ? Container(
-                      height: (MediaQuery.of(context).size.height -
+                      height: (mediaQuery.size.height -
                               appBar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
+                              mediaQuery.padding.top) *
                           0.3,
                       child: Chart(_recentTransactions),
                     )
