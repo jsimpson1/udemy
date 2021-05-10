1f300d2fd3a7bdf2f37ec276ca41d39dd0dda210
Jeremy Simpson <jeremy@accur8software.com>
2021-05-10 12:28:17 -0400

    section 7 module 163 complete

--- academind/deli_meals/lib/category_item.dart
+++ academind/deli_meals/lib/category_item.dart
@@ -1,16 +1,21 @@
+import 'package:deli_meals/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem({this.id, this.title, this.color});
 
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryItem(title, color);
+          return CategoryMealsScreen(
+            categoryId: id,
+            categoryTitle: title,
+          );
         },
       ),
     );
