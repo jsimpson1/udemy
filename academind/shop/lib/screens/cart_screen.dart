85f4d3239c967b485541c25389280041edc53df1
Jeremy Simpson <jeremy@accur8software.com>
2021-07-22 10:33:53 -0400

    section 8 - 206 completed

--- academind/shop/lib/screens/cart_screen.dart
+++ academind/shop/lib/screens/cart_screen.dart
@@ -55,10 +55,11 @@ class CartScreen extends StatelessWidget {
             child: ListView.builder(
               itemCount: cart.itemCount,
               itemBuilder: (ctx, i) => CartItem(
-                cart.items.values.toList()[i].id,
-                cart.items.values.toList()[i].price,
-                cart.items.values.toList()[i].quantity,
-                cart.items.values.toList()[i].title,
+                id: cart.items.values.toList()[i].id,
+                productId: cart.items.keys.toList()[i],
+                price: cart.items.values.toList()[i].price,
+                quantity: cart.items.values.toList()[i].quantity,
+                title: cart.items.values.toList()[i].title,
               ),
             ),
           ),
