c46b3bd0d20470b60c216f6baf30675b945a4a6c
Jeremy Simpson <jeremy@accur8software.com>
2021-07-22 10:44:07 -0400

    section 8 - 209 completed * basic product detail screen added

--- academind/shop/lib/screens/product_detail_screen.dart
+++ academind/shop/lib/screens/product_detail_screen.dart
@@ -4,11 +4,6 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  // final String title;
-  // final double price;
-
-  // ProductDetailScreen(this.title, this.price1);
-
   static const routeName = '/product-detail';
 
   @override
@@ -21,6 +16,42 @@ class ProductDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              // child: Image.network(
+              //   loadedProduct.imageUrl,
+              //   fit: BoxFit.cover,
+              // ),
+              child: Image.asset(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
